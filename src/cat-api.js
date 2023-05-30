f7d22b2648bb12f31decaa9fcf2f6f58273adf6e
Viktoria Rosovska <torimeister@gmail.com>
2023-05-30 19:31:57 +0300

    update homework

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,42 +1,34 @@
 import { BASE_URL, API_KEY, SEARCH_CAT_URL } from './constants';
-import { pError } from './index';
 
-// виконує HTTP-запит і повертає проміс із масивом порід - результатом запиту
+
 function fetchBreeds() {
-  const  params = new URLSearchParams({
+    const params = new URLSearchParams({
         'x-api-key': API_KEY,
     });
 
     return fetch(`${BASE_URL}?${params}`)
-        .then((responce) => {
-            console.log(responce);
-            if (!responce.ok) {
-                throw new Error(responce.status);  
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
             }
-            return responce.json();
-        })
-        .catch(error => {
-            console.log(error);
-            pError.classList.remove('invisible')});
+            return response.json();
+        });
 }
 
-function fetchCatImage(breedId) {
+function fetchCatImageUrl(breedId) {
     const  params = new URLSearchParams({
         'x-api-key': API_KEY,
         breed_ids: breedId,
     });
     return fetch(`${SEARCH_CAT_URL}?${params}`)
-        .then((responce) => {
-            console.log(responce);
-            if (!responce.ok) {
-                throw new Error(responce.status);  
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
             }
-            return responce.json();
-        })
-        .catch(error => {
-            console.log(error);
-            pError.classList.remove('invisible');
+            return response.json();
+        }).then((data) => {
+            return data[0].url;
         });
 }
 
-export { fetchBreeds, fetchCatImage };
+export { fetchBreeds, fetchCatImageUrl };
--- src/index.js
+++ src/index.js
@@ -1,54 +1,100 @@
-import { fetchBreeds, fetchCatImage } from "./cat-api";
+import { fetchBreeds, fetchCatImageUrl } from "./cat-api";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
-const pLoader = document.querySelector('.loader');
-export const pError = document.querySelector('.error');
-pError.classList.add('invisible');
-pLoader.classList.add('invisible');
+// const pError = document.querySelector('.error');
 
-function getBreeds() {
-    fetchBreeds().then((data) => {
-        console.log(data);
-        selectFromCatBreeds(data);
-    });
-}
+const loaderCSS = document.querySelector('.loaderCSS');
+const selectContainer = document.querySelector('.container');
+
+let failed = false;
+
+Notify.init({
+    width: '300px',
+    position: 'center-center',
+    closeButton: false
+});
+
+let breads;
 
 function selectFromCatBreeds(data) {
     data.map(({ id, name }) => {
         return select.add(new Option(name, id));
-    });
-    
-}
-
-function fetchCatByBreed(breedId) {
-    fetchCatImage(breedId).then(data => {
-        console.log(data);
-        const image = document.createElement('img');
-        image.src = data[0].url;
-        image.style.width = '300px';
-        image.style.height = '250px';
-        image.style.objectFit = "contain";
-        catInfo.prepend(image);
-        pLoader.classList.add('invisible');
-    });  
+    }); 
 }
 
 function getCatDescription(breedId) {
-    
-    fetchBreeds().then((data) => {
-        const dataIdx = data.findIndex(({id}) => id === breedId);
-        catInfo.insertAdjacentHTML("beforeend", `<h2>${data[dataIdx].name}</h2><h3>Description:</h3><p>${data[dataIdx].description}</p><h3>Temperament:</h3><p>${data[dataIdx].temperament}</p>`)
-} )};
+    const data = breads;
+    // console.log(data);
+    const dataIdx = data.findIndex(({id}) => id === breedId);
+    catInfo.insertAdjacentHTML("beforeend", `
+        <a href=${catInfoPageUrl(data[dataIdx])} target='_blanc'>
+           <div class="findCatInfo">
+            <div>
+                <h2>${data[dataIdx].name}</h2>
+                <h3>Description:</h3>
+                <p>${data[dataIdx].description}</p>
+                <h3>Temperament:</h3>
+                <p>${data[dataIdx].temperament}</p>
+            </div>
+            <img class="findCat" src="" >
+            </div>
+        </a>`)
+};
+
+function catInfoPageUrl(cat)
+{
+    return cat.cfa_url ??
+        cat.vcahospitals_url ??
+        cat.vetstreet_url ??
+        cat.wikipedia_url;
+}
         
-getBreeds();
+showLoader();
+fetchBreeds().
+    then(response => {
+        breads = response;
+        selectFromCatBreeds(breads);
+        new NiceSelect.bind(select);
+        selectContainer.classList.remove('invisible');
+    }).
+    catch(fatal).
+    finally(hideLoader);
 
 select.addEventListener('change', onChangeSelectForChoiceBreed);
-
 function onChangeSelectForChoiceBreed() {
-    pLoader.classList.remove('invisible');
+    showLoader();
     catInfo.innerHTML = '';
-    // console.log(select.value);
-    fetchCatByBreed(select.value);
     getCatDescription(select.value);
+    fetchCatImageUrl(select.value).
+        then(insertCatImage).
+        catch(fatal).
+        finally(hideLoader);
+}
+
+function insertCatImage(url) {
+    const image = document.querySelector('.findCat');
+    image.src = url;
+    image.style.width = '300px';
+    image.style.height = '300px';
+    image.style.objectFit = "scale-down";
+    // catInfo.append(image);
+}
+
+function showLoader() {
+    loaderCSS.classList.remove('invisible');
+    selectContainer.classList.add('invisible');
+}
+
+function hideLoader() {
+    loaderCSS.classList.add('invisible');
+    if (!failed)
+        selectContainer.classList.remove('invisible');
+}
+
+function fatal(error) {
+    failed = true;
+    // console.log(error);
+    Notify.failure('Oops! Something went wrong! Try to reload the page!');
 }
